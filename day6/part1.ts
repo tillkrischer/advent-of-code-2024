5aabf8b56e9bd22f0011587848c0b9613fe74c47
Till Krischer <till@krischer.io>
2024-12-06 11:51:26 +0100

    day6

---
+++ day6/part1.ts
@@ -0,0 +1,99 @@
+import * as fs from "fs/promises";
+
+type Direction = "up" | "down" | "left" | "right";
+
+type Pos = {
+  x: number;
+  y: number;
+};
+
+// const content = await fs.readFile("day6/test-input.txt", { encoding: "utf8" });
+const content = await fs.readFile("day6/input.txt", { encoding: "utf8" });
+
+const chars = content
+  .split("\n")
+  .filter((l) => l.length > 0)
+  .map((l) => l.split(""));
+
+let pos: Pos | null = getStart(chars);
+let direction: Direction = "up";
+let count = 0;
+
+while (pos !== null) {
+  if (chars[pos.y][pos.x] !== "X") {
+    chars[pos.y][pos.x] = "X";
+    count += 1;
+  }
+  while (isBlocked(chars, pos, direction)) {
+    direction = turn(direction);
+  }
+  pos = getNext(chars, pos, direction);
+}
+
+for (const line of chars) {
+  console.log(line.join(""));
+}
+
+console.log(count);
+
+function turn(direction: Direction): Direction {
+  if (direction === "up") {
+    return "right";
+  }
+  if (direction === "right") {
+    return "down";
+  }
+  if (direction === "down") {
+    return "left";
+  }
+  if (direction === "left") {
+    return "up";
+  }
+}
+
+function isBlocked(chars: string[][], pos: Pos, direction: Direction) {
+  const next = getNext(chars, pos, direction);
+  return next !== null && chars[next.y][next.x] === "#";
+}
+
+function getNext(
+  chars: string[][],
+  pos: Pos,
+  direction: Direction,
+): Pos | null {
+  if (direction === "up" && pos.y === 0) {
+    return null;
+  }
+  if (direction === "down" && pos.y === chars.length - 1) {
+    return null;
+  }
+  if (direction === "left" && pos.x === 0) {
+    return null;
+  }
+  if (direction === "right" && pos.x === chars[0].length) {
+    return null;
+  }
+
+  if (direction === "up") {
+    return { y: pos.y - 1, x: pos.x };
+  }
+  if (direction === "down") {
+    return { y: pos.y + 1, x: pos.x };
+  }
+  if (direction === "left") {
+    return { y: pos.y, x: pos.x - 1 };
+  }
+  if (direction === "right") {
+    return { y: pos.y, x: pos.x + 1 };
+  }
+}
+
+function getStart(chars: string[][]): Pos {
+  for (let y = 0; y < chars.length; y += 1) {
+    for (let x = 0; x < chars[0].length; x += 1) {
+      if (chars[y][x] === "^") {
+        return { x, y };
+      }
+    }
+  }
+}
