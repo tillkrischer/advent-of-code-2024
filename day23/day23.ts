629fe86e20091bf5f2ffdfec8f967aace274856f
Till Krischer <till@krischer.io>
2024-12-23 09:05:26 +0100

    day23 part2

--- day23/day23.ts
+++ day23/day23.ts
@@ -19,21 +19,58 @@ function addEdge(v1: string, v2: string) {
   edges.set(v1, current.add(v2));
 }
 
+function isClique(c: Set<string>) {
+  for (const v of c) {
+    const nbhs = edges.get(v);
+    const edgesAndSelf = new Set([...nbhs, v]);
+    if (!edgesAndSelf.isSupersetOf(c)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+let part2 = "";
 const combs = new Set<string>();
 for (const v of edges.keys()) {
+  const nbhs = edges.get(v);
   if (/^t/.test(v)) {
-    const nbhs = [...edges.get(v)];
-    for (let i = 0; i < nbhs.length; i++) {
-      for (let j = i + 1; j < nbhs.length; j++) {
-        const v1 = nbhs[i];
-        const v2 = nbhs[j];
-        if (v1 !== v2 && edges.get(v1)?.has(v2)) {
-          const comb = [v, v1, v2].toSorted().join(",");
-          combs.add(comb);
-        }
+    for (const subs of getSubsets(nbhs, 2)) {
+      const s = new Set([...subs, v]);
+      if (isClique(s)) {
+        const comb = [...s].toSorted().join(",");
+        combs.add(comb);
       }
+    }
+  }
+  for (const subs of getSubsets(nbhs, 12)) {
+    const s = new Set([...subs, v]);
+    if (isClique(s)) {
+      part2 = [...s].toSorted().join(",");
     }
   }
 }
 const part1 = combs.size;
 console.log(part1);
+console.log(part2);
+
+function* getSubsets(
+  set: Set<string>,
+  size: number,
+): IterableIterator<Set<string>> {
+  if (size === 0) {
+    yield new Set();
+    return;
+  }
+  if (set.size === size) {
+    yield set;
+    return;
+  }
+  const elems = [...set];
+  for (const s of getSubsets(new Set(elems.slice(1)), size)) {
+    yield s;
+  }
+  for (const s of getSubsets(new Set(elems.slice(1)), size - 1)) {
+    yield new Set([elems[0], ...s]);
+  }
+}
